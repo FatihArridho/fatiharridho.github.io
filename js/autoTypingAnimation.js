81b67dba82c7e15051e6d0fc44a6d89116d58bdf
FatihArridho <m.fatiharridho@gmail.com>
2025-03-21 04:15:07 +0700

    geda gedi gedor dor

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hai, Saya Fatih Arridho.</span> Saya adalah seorang pengangguran yang baru tamat dari Sekolah Menengah Atas. Saya sangat tertarik dengan teknologi dan anime, kedua hal tersebut telah melekat pada hidup saya,"
+      ],
+      typeSpeed: 10,
+      contentType: 'html'
+    });
+  }, 100);
